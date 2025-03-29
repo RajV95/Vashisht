6fff0ab16fe54739c74175c77f747beeae6f7c54
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2025-03-29 14:20:11 +0530

    Added admin routes user route to be done

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import adminRouter from "./routes/adminRouter.js";
+import connectCloudinary from "./middleware/cloudinary.js";
+
+app.use(cors());
+app.use(express.json());
+connectCloudinary();
+
+app.use("/api/admin", adminRouter);
+
+// console.log();
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server is running on port ${process.env.PORT}`);
+});
